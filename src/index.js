f18d2adc709f0d2a51f8564b89ae791fee353d1c
Ben Kiboma <omayioben@gmail.com>
2022-06-01 19:07:22 +0300

    Update html file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
+import printMe from './print.js';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  return element;
-}
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-document.body.appendChild(component());
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
